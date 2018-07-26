1ee54170f3c231d827f131a7ba00ffacaac2d9f1
shivamkrj <33348791+shivamkrj@users.noreply.github.com>
2018-07-26 12:22:07 +0530

    Details Activity Implemented

---
+++ app/src/main/java/in/skr/shivamkumar/moviescounter/DetailsScrollingActivity.java
@@ -0,0 +1,13 @@
+package in.skr.shivamkumar.moviescounter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DetailsScrollingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_scrolling);
+    }
+}
